acd0cbc3f3e8419a566db599968e248d57daa439
Christian Gonzalez <82073894+ChrisGonzalez230994@users.noreply.github.com>
2026-08-11 00:23:48 -0300

    Update HeroScreen.tsx

--- src/components/site/HeroScreen.tsx
+++ src/components/site/HeroScreen.tsx
@@ -62,7 +62,7 @@ export function HeroScreen() {
             <img
               src={logoAsset}
               alt="Botbol Clothes"
-              className="h-8 w-auto sm:h-14"
+              className="h-8 w-auto sm:h-10"
             />
           </a>
 
